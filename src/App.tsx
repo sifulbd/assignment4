c02b16d7ec16981356cf4d1051aee7767f516222
Siful Islam <mdsiful.islam@ugent.be>
2025-08-29 13:18:56 +0100

    hook and redux added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const dispatch = useAppDispatch();
+    const count = useAppSelector((state) => state.counter);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const hanndleIncrement = () => {
+        dispatch(increment());
+    };
+    const hanndleDecrement = () => {
+        dispatch(decrement());
+    };
+    console.log(count);
+    return (
+        <div>
+            <h2>Ola, welcome</h2>
+            <span>{count.count}</span>
+            <button onClick={hanndleIncrement}>Increment</button>
+            <button onClick={hanndleDecrement}>Decrement</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ src/redux/hook.ts
@@ -0,0 +1,4 @@
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "./store";
+export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
